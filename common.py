21d68e07f2f73bd4d028dd9e43730fc200daf628
phu <pminimalism2011@gmail.com>
2024-06-06 17:06:38 +0000

    updatre

--- common.py
+++ common.py
@@ -7,7 +7,9 @@ class Config:
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     exp_name = "lqn_mura_fuse_v2"
     data_dir = "./data/"
-    output_dir = "./output/"
+    output_dir = (
+        "/content/drive/MyDrive/Thesis/Sources/storages/mura-fuse/"
+    )
     exp_dir = os.path.join(output_dir, exp_name)
     log_dir = os.path.join(exp_dir, "log/")
     model_dir = os.path.join(exp_dir, "model/")
